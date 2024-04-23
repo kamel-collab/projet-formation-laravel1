10be6b262cf3d6705f48756f165f08f81c513c2c
Kamel Bahmed <kamel.bahmed.info@gmail.com>
2024-04-23 11:59:01 +0100

    ok

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+@extends('template')
+@section('content')
+    <div class="card">
+        <header class="card-header">
+            <h3>Films</h3>
+        </header>
+        <div class="card-body">
+
+            <table class="table is-hoverable">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Titre</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+            </table>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FilmController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('films', FilmController::class);
 Route::get('/', function () {
     return view('welcome');
 });
