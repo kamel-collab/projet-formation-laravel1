ab0124bce27753d072800efc65de42f11b43bd13
Kamel Bahmed <kamel.bahmed.info@gmail.com>
2024-04-23 10:33:30 +0100

    commot

--- app/Models/Film.php
+++ app/Models/Film.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Film extends Model
+class Film extends Model  
 {
     use HasFactory;
+ 
+    protected $fillable=['title','year','description'];
 }
