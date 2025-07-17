887edd0061f670e36df0aefaf2c11f32823999ed
Kamel Bahmed <kamel.bahmed.info@gmail.com>
2025-07-17 08:10:53 +0100

    a

--- database/migrations/2024_04_24_103046_create_categories_table.php
+++ database/migrations/2024_04_24_103046_create_categories_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+//kamel.bahmed.info@gmail.com
 return new class extends Migration
 {
     /**
